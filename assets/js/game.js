8f162292b185245aa4af4ccc3788e54f196f1aa3
Ivona Rieger <ivona.rieger@gmail.com>
2025-01-07 15:03:43 +0000

    js pseudocode comments

---
+++ assets/js/game.js
@@ -0,0 +1,21 @@
+// main container and variables 
+
+//cards data
+
+//score count
+
+// start game
+
+//duplicate array
+
+//shuffle
+
+//generate dards
+
+//flip cards
+
+//check matching
+
+//flip back if not match
+
+//reset and restart
