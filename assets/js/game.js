37948f8d3cd5b2b118eba0651a52b76ccf3e0d2b
Ivona Rieger <ivona.rieger@gmail.com>
2025-01-07 15:15:28 +0000

    added cards in js

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,10 +9,33 @@ var score = 0;
 
 
 //cards data
+var cardData = [
+    { name: "beetle", image: "assets/images/beetle.png" },
+    { name: "chipmunk", image: "assets/images/chipmunk.png" },
+    { name: "hedgehog", image: "assets/images/hedgehog.png" },
+    { name: "ladybug", image: "assets/images/ladybug.png" },
+    { name: "octopus", image: "assets/images/octopus.png" },
+    { name: "parrot", image: "assets/images/parrot.png" },
+    { name: "pigeon", image: "assets/images/pigeon.png" },
+    { name: "seal", image: "assets/images/seal.png" },
+    { name: "skunk", image: "assets/images/skunk.png" },
+    { name: "snake", image: "assets/images/snake.png" },
+    { name: "lizard", image: "assets/images/lizard.png" },
+    { name: "whale", image: "assets/images/whale.png" }
+  ];
+  
 
 //score count
 
 // start game
+initializeGame();
+
+function initializeGame() {
+  // Duplicate array with .concat() instead of spread JS Hint
+  cards = cardData.concat(cardData);
+  shuffleCards();
+  generateCards();
+}
 
 //duplicate array
 
